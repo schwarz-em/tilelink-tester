77ccf740f6a2f48f945980d18a5e510c6f62ebe8
Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>
2024-04-06 14:28:16 -0700

    Add config to pass parameters

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -12,3 +12,11 @@ class TLTesterConfig(p: TesterParams) extends Config((site, here, up) => {
 class DebugTLTConfig0 extends TLTesterConfig(
     new TesterParams(maxInflight=4, beatBytes=4)
 )
+
+class DebugTLTConfig1 extends TLTesterConfig(
+    new TesterParams(maxInflight=1, beatBytes=4)
+)
+
+class TLTConfig(maxInflight: Int, beatBytes: Int) extends TLTesterConfig(
+  new TesterParams(maxInflight=maxInflight, beatBytes=beatBytes)
+)
